8e60721a7b15a3664ba05ee50f8e404c6299506a
unknown <Mateusz.Twardowski@microsoft.wsei.edu.pl>
2022-05-07 12:25:34 +0200

    lab_4

---
+++ Lab_5/Lab_5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class ObservableList1<T>
+    {
+        public void Add()
+        {
+
+        }
+
+        public void Get()
+        {
+
+        }
+
+        public void Set()
+        {
+
+        }
+
+        public void RemoveAt()
+        {
+
+        }
+    }
+}
